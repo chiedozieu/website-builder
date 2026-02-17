fdd834cbc32e5616ee8d7fd5d006bc8c5ff2d743
chiedozieu <chiedozieugwor@gmail.com>
2026-02-17 16:22:53 -0500

    timer update

--- client/src/pages/Loading.tsx
+++ client/src/pages/Loading.tsx
@@ -1,24 +1,20 @@
-import { Loader2 } from 'lucide-react'
-import { useEffect } from 'react'
-
+import { Loader2 } from "lucide-react";
+import { useEffect } from "react";
 
 const Loading = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 6000);
+  }, []);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            window.location.href = "/"
-        }, 6000)
-
-        return () => clearTimeout(timer);
-    }, [])
-    
   return (
-    <div className='h-screen flex flex-col'>
-        <div className="flex items-center justify-center flex-1">
-            <Loader2 className='text-indigo-200 animate-spin size-7'/>
-        </div>
+    <div className="h-screen flex flex-col">
+      <div className="flex items-center justify-center flex-1">
+        <Loader2 className="text-indigo-200 animate-spin size-7" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
